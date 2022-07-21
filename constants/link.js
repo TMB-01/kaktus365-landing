b554e56fcb4fc66a0e96472b2d816c0b9a9aa492
Maraim <maraimtuxtasunov@gmail.com>
2022-07-21 10:28:39 +0500

    changed endpoint

--- constants/link.js
+++ constants/link.js
@@ -1,3 +1,4 @@
-export const ENDPOINT="http://localhost:3456";
+// export const ENDPOINT="http://localhost:3456";
 // export const ENDPOINT="http://78.46.253.40:3456";
+export const ENDPOINT="https://api.kaktus365.uz";
 export const BASE_URL=`${ENDPOINT}/api/v1`;
